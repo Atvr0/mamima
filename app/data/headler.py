3d14bcd33f639d29c2ca4663d22469c1d5863e39
Atvr0 <artvro12345@gmail.com>
2024-09-21 19:27:15 +0300

    first commit

---
+++ app/data/headler.py
@@ -0,0 +1,33 @@
+import json
+
+
+
+
+def get_films(f_path:str = "app/data/films.json") -> list:
+   with open(f_path, "r", encoding="utf-8") as fh:
+        data = json.load(fh)
+        films = data.get("films")
+        return films
+
+
+
+
+def get_film(index: int) -> dict:
+   films = get_films()
+   return films[index]
+
+def save_film(film:dict = {}, f_path:str = "app/data/films.json") -> bool:
+   with open(f_path) as fh:
+       data = json.load(fh)
+
+   films = data.get("films")
+   films.append(film)
+
+
+   with open(f_path, "w") as fh:
+       json.dump(data, fh, ensure_ascii=False, indent=4)
+   return True
+      
+if __name__ == "__main__":
+   print(get_films())
+ 
